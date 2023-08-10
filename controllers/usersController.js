23bde63ab1e5a829a9ff720b16798e47a5abf6af
Patricio Sirena <patobsirena@gmail.com>
2023-08-10 20:06:11 -0300

    primera versión

---
+++ controllers/usersController.js
@@ -0,0 +1,51 @@
+const { request, response } = require('express');
+const Usuario = require('../models/usuarios')
+const bcrypt = require('bcrypt')
+const index = async (req = request, res = response) => {
+    let usuarios = await Usuario.find()
+    res.status(200).json({ usuarios: usuarios });
+}
+const getOne = async (req = request, res = response) => {
+    let usuario = await Usuario.findOne();
+    res.status(200).json({ usuario });
+}
+const update = async (req = request, res = response) => {
+    const { id } = req.params;
+    const { password, email, ...resto } = req.body;
+    if (password) {
+        let passEncrip = bcrypt.hashSync(password, 12);
+        resto.password = passEncrip;
+    }
+    const usuario = await Usuario.findByIdAndUpdate(id, resto, { new: true });
+    res.status(201).json({ usuario: usuario, msg: "Usuario actualizo con éxito!" })
+}
+const create = async (req = request, res = response) => {
+    const { firstName, lastName, email, password } = req.body;
+    console.log(req.body);
+    let passEncrip = bcrypt.hashSync(password, 12);
+    const newUser = new Usuario({ firstName, lastName, email, password: passEncrip })
+    try {
+        await newUser.save();
+        return res.status(201).json({ msg: "Usuario creado con éxito!" })
+    } catch (error) {
+        return res.json({ error })
+    }
+
+}
+const del = async (req = request, res = response) => {
+    const { id } = req.params;
+    const usuario = await Usuario.findById(id);
+    if (!usuario.activo) {
+        return res.json({ mgs: "El usuario está inactivo!" })
+    }
+    const usuaBorrado = await Usuario.findByIdAndUpdate(id, { activo: false }, { new: true });
+
+    res.status(200).json({ msg: "El usuario pasó a estar inactivo", data: usuaBorrado })
+}
+module.exports = {
+    index,
+    getOne,
+    update,
+    create,
+    del
+}
