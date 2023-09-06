5ba17285ff1a7b7c72cad3c388993524e427d3dc
nanosp7 <spectormariano1@gmail.com>
2023-09-06 20:11:56 -0300

    test1

--- controllers/usersController.js
+++ controllers/usersController.js
@@ -1,51 +1,76 @@
-const { request, response } = require('express');
+const { request, response } = require('express')
 const Usuario = require('../models/usuarios')
 const bcrypt = require('bcrypt')
+const {esAdmin} = require('../helpers/db-validator')
+
 const index = async (req = request, res = response) => {
-    let usuarios = await Usuario.find()
-    res.status(200).json({ usuarios: usuarios });
+    const { desde = 0, limite = 10 } = req.query
+    const sentencia = { activo: true }
+    // let usuarios = await Usuario.find()
+    const [total, urs] = await Promise.all([
+        Usuario.countDocuments(sentencia),
+        Usuario.find(sentencia).skip(desde).limit(limite)
+    ])
+    res.status(200).json({ total, urs })
 }
 const getOne = async (req = request, res = response) => {
-    let usuario = await Usuario.findOne();
-    res.status(200).json({ usuario });
+    let { id } = req.params
+    let usuario = await Usuario.findOne({ _id: id })
+    res.status(200).json({ usuario })
 }
 const update = async (req = request, res = response) => {
-    const { id } = req.params;
-    const { password, email, ...resto } = req.body;
+    const { id } = req.params
+    const { password, email, ...resto } = req.body
     if (password) {
-        let passEncrip = bcrypt.hashSync(password, 12);
-        resto.password = passEncrip;
+        let passEncrip = bcrypt.hashSync(password, 12)
+        resto.password = passEncrip
     }
-    const usuario = await Usuario.findByIdAndUpdate(id, resto, { new: true });
-    res.status(201).json({ usuario: usuario, msg: "Usuario actualizo con éxito!" })
+    const usuario = await Usuario.findByIdAndUpdate(id, resto, { new: true })
+    res.status(201).json({ usuario: usuario, msg: "Usuario actualizado con exito!" })
 }
 const create = async (req = request, res = response) => {
-    const { firstName, lastName, email, password } = req.body;
-    console.log(req.body);
-    let passEncrip = bcrypt.hashSync(password, 12);
-    const newUser = new Usuario({ firstName, lastName, email, password: passEncrip })
-    try {
-        await newUser.save();
-        return res.status(201).json({ msg: "Usuario creado con éxito!" })
-    } catch (error) {
-        return res.json({ error })
-    }
+    const id = req.body.id
+    esAdmin(id)
+    // const { firstName, lastName, email, password } = req.body;
+    // let passEncrip = bcrypt.hashSync(password, 12)
+    // const newUser = new Usuario({ firstName, lastName, email, password: passEncrip })
+    // try {
+    //     await newUser.save()
+    //     return res.status(200).json({ msg: "Usuario creado con exito!" })
+    // } catch (error) {
+    //     return res.json({ error })
+    // }
+    res.send(esAdmin(id))
 
 }
 const del = async (req = request, res = response) => {
-    const { id } = req.params;
-    const usuario = await Usuario.findById(id);
+    const { id } = req.params
+    const usuario = await Usuario.findById(id)
     if (!usuario.activo) {
-        return res.json({ mgs: "El usuario está inactivo!" })
+        return res.json({ msg: "El usuario esta inactivo" })
     }
-    const usuaBorrado = await Usuario.findByIdAndUpdate(id, { activo: false }, { new: true });
+    const usuarioBorrado = await Usuario.findByIdAndUpdate(id, { activo: false }, { new: true })
 
-    res.status(200).json({ msg: "El usuario pasó a estar inactivo", data: usuaBorrado })
+    res.status(200).json({ msg: "El usuaio paso a estar inactivo", data: usuarioBorrado })
 }
+const activeUser = async (req = request, res = response) => {
+    const { id } = req.params
+    const urs = await Usuario.findOne({ _id: id })  
+    if (!urs.activo) {
+        const usuario = await Usuario.findByIdAndUpdate(id, { activo: true }, { new: true })
+        res.status(201).json({ usuario: usuario, msg: "Usuario actualizado con exito!" })
+    }
+    else {
+        const usuario = await Usuario.findByIdAndUpdate(id, { activo: false }, { new: true })
+        res.status(201).json({ usuario: usuario, msg: "Usuario inactivo" })
+    }
+}
+
 module.exports = {
     index,
     getOne,
     update,
     create,
-    del
+    del,
+    activeUser
 }
--- database/config.js
+++ database/config.js
@@ -1,14 +1,15 @@
 const mongoose = require('mongoose');
 
-const dbConnection =async ()=>{
-    console.log(process.env.MONGO_DB_CNN);
+const dbConnection = async () => {
     try {
         await mongoose.connect(process.env.MONGO_DB_CNN);
-        console.log("estamos conectados a la base de  datos");
+        console.log("Estamos conectados a la base de datos");
     } catch (error) {
         throw new Error("Error a la hora de iniciar la base de datos");
     }
 }
-module.exports={
+module.exports = {
     dbConnection
 }
+
+//test1
