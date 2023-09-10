85ec273a2e86c662d8a37adf3a8f65908db3abd3
solheredia <solheredia.555@gmail.com>
2023-09-10 12:26:56 -0300

    Update usuarios.js

--- routes/usuarios.js
+++ routes/usuarios.js
@@ -1,9 +1,9 @@
 const { Router } = require("express")
 const router = Router()
 const usersControllers = require('../controllers/usersController')
-const {validarCampos} = require('../middlewares/validar_campos')
-const {check} = require('express-validator')
-const {usuarioExiste} = require('../helpers/db-validator')
+const { validarCampos } = require('../middlewares/validar_campos')
+const { check } = require('express-validator')
+const { usuarioExiste } = require('../helpers/db-validator')
 
 router.get("/getUsers", usersControllers.index)
 router.get("/getOneUser/:id", [
@@ -11,9 +11,19 @@ router.get("/getOneUser/:id", [
     check("id").custom(usuarioExiste),
     validarCampos
 ], usersControllers.getOne)
-router.put("/updateUser/:id",validarCampos, usersControllers.update)
-router.post("/createUser/",validarCampos, usersControllers.create)
-router.delete("/deleteUser/:id", usersControllers.del)
-router.put("/activeUser/:id",validarCampos, usersControllers.activeUser)
+router.put("/updateUser/:id", validarCampos, usersControllers.update)
+router.post("/createUser/", [
+    check("firstName","El nombre es obligatorio"),
+    check("lastName","El apellido es obligatorio"),
+    check("email","No es un correo valido").isEmail,
+    check("password","La contraseña debe tener un minimo de 8 caracteres").isLength({min:8}),
+    validarCampos
+], usersControllers.create)
+router.delete("/deleteUser/:id", [
+    check("id", "No es un id Válido").isMongoId(),
+    check("id").custom(usuarioExiste),
+    validarCampos
+], usersControllers.del)
+router.put("/activeUser/:id", validarCampos, usersControllers.activeUser)
 
 module.exports = router
