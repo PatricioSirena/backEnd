8d65768c2ff5fc16e3bd5e3a608063777f40cc64
solheredia <solheredia.555@gmail.com>
2023-09-11 18:03:28 -0300

    CRUD finalizado

--- controllers/productController.js
+++ controllers/productController.js
@@ -1,17 +1,36 @@
+const productos = require('../models/productos');
 const Producto = require('../models/productos')
 
 const index = async (req, res) => {
-    let productos = await Producto.findOneAndUpdate()
-    console.log(productos);
-    res.status(200).json({ data: productos })
+    try {
+        const productos = await Producto.find();
+        return res.status(200).json({ data: productos })
+    } catch (error) {
+        return res.json({ error })
+    }
 }
 
-const getOne = (req, res) => {
-    res.status(200).json({ msg: "Realizaste una petición getOneProduct" })
+const getOne = async (req, res) => {
+    const { id } = req.params
+    try {
+        const producto = await productos.findById(id)
+        return res.status(200).json({ producto })
+    } catch (error) {
+        return res.json({ error })
+    }
 }
 
-const update = (req, res) => {
-    res.status(200).json({ msg: "Realizaste una petición updateProduct" })
+const update = async (req, res) => {
+    const { id } = req.params
+    const { name, description, price, src } = req.body
+    const productoNew = { name, description, price: Number(price), src }
+    console.log(productoNew);
+    try {
+        let newProduct = await Producto.findByIdAndUpdate(id, productoNew, { new: true })
+        return res.status(201).json({ newProduct })
+    } catch (error) {
+        return res.json({ error })
+    }
 }
 
 const create = async (req, res) => {
@@ -28,8 +47,15 @@ const create = async (req, res) => {
     }
 }
 
-const del = (req, res) => {
-    res.status(200).json({ msg: "Realizaste una petición deleteProduct" })
+const del = async (req, res) => {
+    const { id } = req.params
+    console.log(id);
+    try {
+        await Producto.findByIdAndDelete(id)
+        return res.status(200).json({msg: "Se elimino con éxito el producto"})
+    } catch (error) {
+        return res.json({ error });
+    }
 }
 module.exports = {
     index,
