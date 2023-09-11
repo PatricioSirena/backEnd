fb2c95399a4f9d1f6af027c30e45f1ddc457460c
solheredia <solheredia.555@gmail.com>
2023-09-11 16:31:29 -0300

    manteinment

--- controllers/productController.js
+++ controllers/productController.js
@@ -1,7 +1,9 @@
 const Producto = require('../models/productos')
 
-const index = (req, res) => {
-    res.status(200).json({ msg: "Realizaste una petición getProducts" })
+const index = async (req, res) => {
+    let productos = await Producto.findOneAndUpdate()
+    console.log(productos);
+    res.status(200).json({ data: productos })
 }
 
 const getOne = (req, res) => {
@@ -13,16 +15,16 @@ const update = (req, res) => {
 }
 
 const create = async (req, res) => {
-    console.log(req.body);
-    let {name, description, price, src} = req.body
-    let producto =  {name, description, price, src}
+
+    let { name, description, price, src } = req.body;
+    let producto = { name, description, price, src };
     const newProducto = await new Producto(producto)
     console.log(newProducto);
     try {
-        await newProducto.save()
-        return res.status(201).json({msg: "Producto creado con éxito"})
+        await newProducto.save();
+        return res.status(201).json({ mgs: 'Producto creado con éxito' })
     } catch (error) {
-        return res.json({error})
+        return res.json({ error });
     }
 }
 
--- models/productos.js
+++ models/productos.js
@@ -1,31 +1,30 @@
 /*
-name, description, price, src
-*/
-
-const { Schema, model } = require('mongoose') 
-const ProductosSchema = Schema({
-    name: {
-        type: String,
-        required: [true, "El nombre es obligatorio"]
-    },
-    description: {
-        type: String,
-        required: [true, "La descripcion es obligatoria"]
-    },
-    price: {
-        type: Number,
-        required: [true, "El precio es obligatorio"]
-    },
-    src: {
-        type: String,
-        required: [true, "La imagen es obligatoria"]
-    }
-})
-
-ProductosSchema.methods.toJSON = function () {
-    const {__v, _id, ...producto} = this.toObject()
-    producto.uid = _id
-    return producto
-}
-
-module.exports = model("Producto" , ProductosSchema)
+ "name" "descripction" "price" "src"
+ */
+ const {Schema,model} =require('mongoose');
+ const ProductosSchema = Schema({
+     name:{
+         type:String,
+         required:[true, "El nombre es obligatorio"]
+     },
+     description:{
+         type:String,
+         required:[true, "La descripción es obligatoria"]
+     },
+     price:{
+         type:Number,
+         required:[true, "El precio es obligatorio"]
+     },
+     src:{
+         type:String,
+         required:[true, "La imagen del producto es obligatoria"]
+     },
+     
+ });
+ ProductosSchema.methods.toJSON = function () {
+     const {__v, _id,...producto} = this.toObject();
+     producto.id = _id;
+     return producto
+ };
+ module.exports = model("Producto",ProductosSchema);
+ 
