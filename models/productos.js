06074bff3b380d1a7c6e35eacd196ec62f957bde
solheredia <solheredia.555@gmail.com>
2023-09-26 21:19:54 -0300

    Update productos.js

--- models/productos.js
+++ models/productos.js
@@ -1,30 +1,41 @@
 /*
  "name" "descripction" "price" "src"
  */
- const {Schema,model} =require('mongoose');
- const ProductosSchema = Schema({
-     name:{
-         type:String,
-         required:[true, "El nombre es obligatorio"]
-     },
-     description:{
-         type:String,
-         required:[true, "La descripción es obligatoria"]
-     },
-     price:{
-         type:Number,
-         required:[true, "El precio es obligatorio"]
-     },
-     src:{
-         type:String,
-         required:[true, "La imagen del producto es obligatoria"]
-     },
-     
- });
- ProductosSchema.methods.toJSON = function () {
-     const {__v, _id,...producto} = this.toObject();
-     producto.id = _id;
-     return producto
- };
- module.exports = model("Producto",ProductosSchema);
- 
+const { Schema, model } = require('mongoose');
+const ProductosSchema = Schema({
+    id: {
+        type: Number,
+        required: [true, "El id es Obligatorio"]
+    },
+    titulo: {
+        type: String,
+        required: [true, "El nombre es obligatorio"]
+    },
+    texto: {
+        type: String,
+        required: [true, "El texto es obligatoria"]
+    },
+    categoria:{
+        type:String,
+        required:[true, "La categoria es obligatoria"]
+    },
+    precio: {
+        type: Number,
+        required: [true, "El precio es obligatorio"]
+    },
+    imagen: {
+        type: String,
+        required: [true, "La imagen del producto es obligatoria"]
+    },
+    activo: {
+        type: String,
+        required: [true]
+    },
+
+});
+ProductosSchema.methods.toJSON = function () {
+    const { __v, _id, ...producto } = this.toObject();
+    producto.id = _id;
+    return producto
+};
+module.exports = model("Producto", ProductosSchema);
