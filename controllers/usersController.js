648490e9bfaf0ce6b4db2ccb47214ccd218c248c
Patricio Sirena <patobsirena@gmail.com>
2023-10-14 21:25:21 -0300

    Cambio en controlador
    Modifico el index del controlador para ver si de esta forma me muestra todos los usuarios en deploy

--- controllers/usersController.js
+++ controllers/usersController.js
@@ -1,22 +1,35 @@
 const {request,response}=require('express');
+const usuarios = require('../models/usuarios');
 const Usuario = require('../models/usuarios');
 const {esAdmin} = require('../helpers/db-validator');
 const bcrypt = require('bcrypt');
-const  index=async(req = request, res=response)=>{
-  const{desde=0,limite=5}=req.query;
-  const sentencia={activo:true};
-  // let usuarios= await Usuario.find()
-  const [total,usr]= await Promise.all([
-    Usuario.countDocuments(sentencia),
-    Usuario.find(sentencia).skip(desde).limit(limite)
-  ])
-  res.status(200).json({total,usr});
+
+// const  index=async(req = request, res=response)=>{
+//   const{desde=0,limite=5}=req.query;
+//   const sentencia={activo:true};
+//   // let usuarios= await Usuario.find()
+//   const [total,usr]= await Promise.all([
+//     Usuario.countDocuments(sentencia),
+//     Usuario.find(sentencia).skip(desde).limit(limite)
+//   ])
+//   res.status(200).json({total,usr});
+// }
+
+const index = async (req, res) => {
+  try {
+      const usuarios = await Usuario.find();
+      return res.status(200).json({ data: usuarios })
+  } catch (error) {
+      return res.json({ error })
+  }
 }
+
 const getOne=async(req=request, res=response)=>{
   let {id}=req.params;
   let usuario= await Usuario.findOne({_id:id});
   res.status(200).json({usuario});
 }
+
 const update=async(req=request, res=response)=>{
   const {id} =req.params;
   const {password,correo, ...resto}=req.body;
@@ -28,6 +41,7 @@ const update=async(req=request, res=response)=>{
   const usuario = await Usuario.findByIdAndUpdate(id,resto,{new:true});
   res.status(201).json({usuario:usuario,msg:"Usuario actualizo con éxito!"})
 }
+
 const create=async(req=request, res=response)=>{
   // const id = req.body.id;
   // esAdmin(id)
@@ -43,6 +57,7 @@ const create=async(req=request, res=response)=>{
   }
   
 }
+
 // const del=async(req=request, res=response)=>{
 //   const {id} =req.params;
 //   const usuario = await Usuario.findById(id);
@@ -53,6 +68,7 @@ const create=async(req=request, res=response)=>{
 
 //   res.status(200).json({msg:"El usuario pasó a estar inactivo",data:usuaBorrado})
 // }
+
 const activeUser=async(req=request, res=response)=>{
   const {id} =req.params;
   const urs = await Usuario.findOne({_id:id});
@@ -64,6 +80,7 @@ const activeUser=async(req=request, res=response)=>{
     res.status(201).json({usuario:usuario,msg:"Usuario inactivo!"})
   }
 }
+
 module.exports={
   index,
   getOne,
