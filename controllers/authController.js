e191b012701147e930187f2e9262c649d733bfd5
Patricio Sirena <patobsirena@gmail.com>
2023-10-18 20:05:24 -0300

    .

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,4 +1,4 @@
-const bcryt = require('bcrypt');
+const bcrypt = require('bcrypt');
 const Usuario = require('../models/usuarios');
 const {generarJWT} =require('../helpers/generar-jwt')
 const login=async(req,res)=>{
@@ -7,7 +7,7 @@ const login=async(req,res)=>{
         const user = await Usuario.findOne({correo:correo})
         console.log(user);
         if(user){
-            if(bcryt.compareSync(user.password,password)){
+            if(bcrypt.compareSync(password,user.password)){
                 console.log("Usuario autenticado!");
                 const token = await generarJWT(user.uid);
                 return res.status(200).json({
