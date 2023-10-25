0f0b6be5ce005ebf558aa356110c0fc44000faad
Patricio Sirena <patobsirena@gmail.com>
2023-10-25 09:18:37 -0300

    Cambio controladores de autenticación
    Retornó usuario en casos que este mal la contraseña o que no coincida con la base de datos

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,33 +1,37 @@
 const bcrypt = require('bcrypt');
 const Usuario = require('../models/usuarios');
-const {generarJWT} =require('../helpers/generar-jwt')
-const login=async(req,res)=>{
-    let {password,correo}=req.body;
+const { generarJWT } = require('../helpers/generar-jwt')
+const login = async (req, res) => {
+    let { password, correo } = req.body;
     try {
-        const user = await Usuario.findOne({correo:correo})
-        if(user){
-            if(bcrypt.compareSync(password,user.password)){
+        const user = await Usuario.findOne({ correo: correo })
+        if (user) {
+            if (bcrypt.compareSync(password, user.password)) {
                 console.log("Usuario autenticado!");
                 const token = await generarJWT(user.uid);
                 return res.status(200).json({
                     user,
                     token
                 })
-            }else{
-                return res.status(401).json({msg:"datos incorrectos!"})
+            } else {
+                return res.status(401).json({
+                    msg: "datos incorrectos!",
+                    user
+                })
             }
-        }else{
+        } else {
             return res.status(404).json({
-                msg:"Usuario no existe!"
+                msg: "Usuario no existe!",
+                user
             })
         }
     } catch (error) {
         return res.status(500).json({
-            msg:"Contacta al administrador"
+            msg: "Contacta al administrador"
         })
     }
-    
+
 }
-module.exports={
+module.exports = {
     login
 }
