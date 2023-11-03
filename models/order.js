6f670a5905f2a37f080edaaa28470006371cfb53
Patricio Sirena <patobsirena@gmail.com>
2023-11-03 09:42:18 -0300

    Cambios en modelo de pedidos

--- models/order.js
+++ models/order.js
@@ -1,9 +1,9 @@
 const {Schema,model} =require('mongoose');
 const OrderSchema = Schema({
-    usuario:{
-        type:String,
-        required:[true, "El usuario es obligatorio"]
-    },
+    // usuario:{
+    //     type:String,
+    //     required:[true, "El usuario es obligatorio"]
+    // },
     platos:{
         type: Array,
         default: []
@@ -18,7 +18,7 @@ const OrderSchema = Schema({
     }
 })
 OrderSchema.methods.toJSON = function () {
-    const {__v,_id,...order} = this.toObject();
+    const {_id,...order} = this.toObject();
     order.id = _id;
     return order
 };
